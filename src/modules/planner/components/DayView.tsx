ec4700e26c3cc46eb666d84aa9ffdb0f693f471b
Obatola Seward-Evans <oba.seward@gmail.com>
2023-03-02 10:27:36 -0500

    light styling

--- src/modules/planner/components/DayView.tsx
+++ src/modules/planner/components/DayView.tsx
@@ -1,17 +1,18 @@
 import moment from "moment";
 import { usePlanner } from "../hooks/usePlannerContext";
 import { TasksView } from "./TasksView";
+import styles from "../styles/DayView.module.css";
 
 export const DayView = () => {
     const { state } = usePlanner();
 
     return (
-        <>
+        <div className={styles.wrapper}>
             {moment(state.currentDayId)
                 .startOf("day")
                 .format("dddd, MMMM Do YYYY")}
             <TasksView isPriority />
             <TasksView />
-        </>
+        </div>
     );
 };
